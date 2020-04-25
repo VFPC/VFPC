c23713830f3fe2d66596e700f4d57ac4ff6a0421
hpeter <hendrik.peter@vodafone.de>
2020-04-25 23:16:05 +0200

    Version 2.0
    - Replaced TXT with JSON
    - Added Config for EDDB, EDDH, EDDP, EDDT, EDDV, EDDW
    - Multiple Airports
    - Multiple conditions per SID
    - Added Condition: Destination Airport
    - Added Condition: Airways
    - Added E/O option: ANY

--- analyzeFP.cpp
+++ analyzeFP.cpp
@@ -36,7 +36,7 @@ CVFPCPlugin::CVFPCPlugin(void) :CPlugIn(EuroScopePlugIn::COMPATIBILITY_CODE, MY_
 	GetModuleFileNameA(HINSTANCE(&__ImageBase), DllPathFile, sizeof(DllPathFile));
 	pfad = DllPathFile;
 	pfad.resize(pfad.size() - strlen("VFPC.dll"));
-	pfad += "Sid.txt";
+	pfad += "Sid.json";
 
 	debugMode = false;
 	initialSidLoad = false;
@@ -68,192 +68,236 @@ void CVFPCPlugin::sendMessage(string message) {
 	DisplayUserMessage("Message", "VFPC", message.c_str(), true, true, true, false, false);
 }
 
-// Get data from sid.txt
-void CVFPCPlugin::getSids(string airport) {
-	sidDatei.open(pfad, ios::in);
-	if (!sidDatei) {	// Display Error, if something bad happened.
+void CVFPCPlugin::getSids() {
+	stringstream ss;
+	ifstream ifs;
+	ifs.open(pfad.c_str(), ios::binary);
+	ss << ifs.rdbuf();
+	ifs.close();
+
+	if (config.Parse<0>(ss.str().c_str()).HasParseError()) {
 		string error{ pfad };
 		error += " couldn't be opened!";
 		sendMessage("Error", error.c_str());
-	} else {
-		sendMessage("Loading Sids...");
-		string read, readbuffer;
-		string sid[4];			// 0 = Name, 1 = Even/Odd, 2 = Min FL, 3 = Max FL
-		if (airport == "") {
-			airport = ControllerMyself().GetCallsign();
-		}
-		airport.resize(4);	// Take e.g. "EDDF_TWR", or "EDDF_N_APP" and make it to "EDDF"
-		sendMessage("Loading", airport);
-		// To-Do: safing setting for multiple airports
-
-		while (getline(sidDatei, read)) {	// Read the line! Love isn't always on time!
-			readbuffer = read;
-			readbuffer.resize(4);
-			debugMessage("Reading", read);
-			debugMessage("Readbuffer", readbuffer);
-			if (readbuffer == airport) {
-				sid[0] = read;  // sid[0] = "EDDF;OBOKA;Even;0;0";
-				sid[0].erase(0, sid[0].find_first_of(';') + 1);   //sid[0] = "OBOKA;Even;0;0";
-				debugMessage("Sid[0]", sid[0]);
-				sid[1] = sid[0];
-				sid[0].erase(sid[0].find_first_of(';'), sid[0].length());   //sid[0] = "OBOKA";
-				sid[1].erase(0, sid[1].find_first_of(';') + 1);	//sid[1] = Even;0;0";
-				debugMessage("Sid[1]", sid[1]);
-				sid[2] = sid[1];
-				sid[1].erase(sid[1].find_first_of(';'), sid[1].length());
-				sid[2].erase(0, sid[2].find_first_of(';') + 1);
-				debugMessage("Sid[2]", sid[2]);
-				sid[3] = sid[2];
-				sid[2].erase(sid[2].find_first_of(';'), sid[2].length());
-				sid[3].erase(0, sid[3].find_first_of(';') + 1);
-				debugMessage("Sid[3]", sid[3]);
-				//sid[3].erase(sid[3].find_first_of(';'), sid[3].length());
-				// To-Do: conditional SIDs
-
-				// Adding all the data to the vectors
-				sidName.push_back(sid[0]);
-				sidEven.push_back(sid[1]);
-				sidMin.push_back(stoi(sid[2]));
-				sidMax.push_back(stoi(sid[3]));
-
-				// Showing what is added to the sid-vectors
-				if (debugMode) {
-					string sidContainer{ sid[0] };
-					sidContainer += sid[1];
-					sidContainer += sid[2];
-					sidContainer += sid[3];
-
-					debugMessage("Adding", sidContainer);
-				}
-			}
-		}
-
-		sidDatei.close();
-		string sidCount{ "SID loading finished: " };
-		sidCount += to_string(sidName.size());
-		sidCount += " SIDs loaded.";
-		sendMessage(sidCount.c_str());
+		return;
 	}
-	// Output of all vectors with their data in it
-	if (debugMode) {
-		string names{};
-		for (string buf : sidName)
-		{
-			names += buf;
-			names += ";";
-		}
-		string even{};
-		for (string buf : sidEven)
-		{
-			even += buf;
-			even += ";";
-		}
-		string min{};
-		for (int buf : sidMin)
-		{
-			min += to_string(buf);
-			min += ";";
-		}
-		string max{};
-		for (int buf : sidMax)
-		{
-			max += to_string(buf);
-			max += ";";
-		}
-		debugMessage("Sid Name", names);
-		debugMessage("Sid Even", even);
-		debugMessage("Sid Min", min);
-		debugMessage("Sid Max", max);
+
+	airports.clear();
+
+	for (SizeType i = 0; i < config.Size(); i++) {
+		const Value& airport = config[i];
+		string airport_icao = airport["icao"].GetString();
+
+		airports.insert(pair<string, SizeType>(airport_icao, i));
 	}
 }
 
 // Does the checking and magic stuff, so everything will be alright, when this is finished! Or not. Who knows?
 vector<string> CVFPCPlugin::validizeSid(CFlightPlan flightPlan) {
 	vector<string> returnValid{};		// 0 = Callsign, 1 = valid/invalid SID, 2 = SID Name, 3 = Even/Odd, 4 = Minimum Flight Level, 5 = Maximum Flight Level, 6 = Passed
-	string FlightPlanString = flightPlan.GetFlightPlanData().GetRoute();
-	int RFL = flightPlan.GetFlightPlanData().GetFinalAltitude();
 	returnValid.push_back(flightPlan.GetCallsign());
 	bool valid{ false };
-	for (int i = 0; i < sidName.size(); i++) {
-		bool passed[3]{ false };
-		if (FlightPlanString.find(sidName.at(i)) != string::npos) {
-			valid = true;
-			returnValid.push_back("Valid");	
-			returnValid.push_back(sidName.at(i));
-			if (sidEven.at(i) == "Even") {
-				if ((RFL / 1000) % 2 == 0) {
-					returnValid.push_back("Passed Even");
-					passed[0] = true;
-				} else {
-					returnValid.push_back("Failed Even");
-				}
+
+	string origin = flightPlan.GetFlightPlanData().GetOrigin(); to_upper(origin);
+	string destination = flightPlan.GetFlightPlanData().GetDestination(); to_upper(destination);
+	SizeType origin_int;
+	int RFL = flightPlan.GetFlightPlanData().GetFinalAltitude();
+	
+	vector<string> route = split(flightPlan.GetFlightPlanData().GetRoute(), ' ');
+	for (int i = 0; i < route.size(); i++) {
+		to_upper(route[i]);
+	}
+
+	string sid = flightPlan.GetFlightPlanData().GetSidName(); to_upper(sid);
+	string first_wp = sid.substr(0, sid.find_first_of("0123456789")); to_upper(first_wp);
+	string first_airway;
+
+	vector<string>::iterator it = find(route.begin(), route.end(), first_wp);
+	if (it != route.end() && (it - route.begin()) != route.size() - 1) {
+		first_airway = route[(it - route.begin()) + 1];
+		to_upper(first_airway);
+	}
+
+	// Airport defined
+	if (airports.find(origin) == airports.end()) {
+		returnValid.push_back("Invalid");
+		returnValid.push_back("No valid Airport found!");
+		for (int i = 0; i < 5; i++) {
+			returnValid.push_back("-");
+		}
+		returnValid.push_back("Failed");
+		return returnValid;
+	}
+	else
+		origin_int = airports[origin];
+
+	// Any SIDs defined
+	if (!config[origin_int].HasMember("sids") || config[origin_int]["sids"].IsArray()) {
+		returnValid.push_back("Invalid");
+		returnValid.push_back("No SIDs defined!");
+		for (int i = 0; i < 5; i++) {
+			returnValid.push_back("-");
+		}
+		returnValid.push_back("Failed");
+		return returnValid;
+	}
+
+	// Needed SID defined
+	if (!config[origin_int]["sids"].HasMember(first_wp.c_str()) || !config[origin_int]["sids"][first_wp.c_str()].IsArray()) {
+		returnValid.push_back("Invalid");
+		returnValid.push_back("No valid SID found!");
+		for (int i = 0; i < 5; i++) {
+			returnValid.push_back("-");
+		}
+		returnValid.push_back("Failed");
+		return returnValid;
+	}
+
+	const Value& conditions = config[origin_int]["sids"][first_wp.c_str()];
+	for (SizeType i = 0; i < conditions.Size(); i++) {
+		returnValid.clear();
+		returnValid.push_back(flightPlan.GetCallsign());
+		bool passed[5]{ false };
+		valid = false;
+
+
+		// Does Condition contain our destination if it's limited
+		if (conditions[i]["destinations"].IsArray() && conditions[i]["destinations"].Size()) {
+			if (arrayContains(conditions[i]["destinations"], destination.c_str())) {
+				returnValid.push_back("Passed Destination");
+				passed[0] = true;
 			}
-			else if (sidEven.at(i) == "Odd") {
-				if ((RFL / 1000) % 2 != 0) {
-					returnValid.push_back("Passed Odd");
-					passed[0] = true;
-				} else {
-					returnValid.push_back("Failed Odd");
-				}
+			else {
+				continue;
 			}
-			else if (sidEven.at(i) != "Even" && sidEven.at(i) != "Odd") {
-				string errorText{ "Config Error for Even/Odd on SID: " };
-				errorText += sidName.at(i);
-				sendMessage("Error", errorText);
-				returnValid.push_back("Config Error for Even/Odd on this SID!");
-			}
-			if (sidMin.at(i) != 0) {
-				if ((RFL / 100) >= sidMin.at(i)) {
-					returnValid.push_back("Passed Minimum Flight Level");
-					passed[1] = true;
-				}
-				else {
-					returnValid.push_back("Failed Minimum Flight Level");
-				}
-			} else {
-				returnValid.push_back("No Minimum Flight Level");
+		}
+		else {
+			returnValid.push_back("No Destination restriction");
+			passed[0] = true;
+		}
+
+
+		// Does Condition contain our first airway if it's limited
+		if (conditions[i]["airways"].IsArray() && conditions[i]["airways"].Size()) {
+			string rte = flightPlan.GetFlightPlanData().GetRoute();
+			if (routeContains(rte, conditions[i]["airways"])) {
+				returnValid.push_back("Passed Airways");
 				passed[1] = true;
 			}
-			if (sidMax.at(i) != 0) {
-				if ((RFL / 100) <= sidMax.at(i)) {
-					returnValid.push_back("Passed Maximum Flight Level");
-					passed[2] = true;
-				}
-				else {
-					returnValid.push_back("Failed Maximum Flight Level");
-				}
-			} else {
-				returnValid.push_back("No Maximum Flight Level");
+			else {
+				continue;
+			}
+		}
+		else {
+			returnValid.push_back("No Airway restriction");
+			passed[1] = true;
+		}
+
+		valid = true;
+		returnValid.insert(returnValid.begin() + 1, "Valid");
+		returnValid.insert(returnValid.begin() + 2, first_wp);
+
+		// Direction of condition
+		string direction = conditions[i]["direction"].GetString();
+		to_upper(direction);
+
+		if (direction == "EVEN") {
+			if ((RFL / 1000) % 2 == 0) {
+				returnValid.push_back("Passed Even");
 				passed[2] = true;
 			}
-			bool passedVeri{ false };
-			for (int i = 0; i < 3; i++) {
-				if (passed[i])
-				{
-					passedVeri = true;
-				} else {
-					passedVeri = false;
-					break;
-				}
+			else {
+				returnValid.push_back("Failed Even");
 			}
-			if (passedVeri) {
-				returnValid.push_back("Passed");
-			} else {
-				returnValid.push_back("Failed");
+		}
+		else if (direction == "ODD") {
+			if ((RFL / 1000) % 2 != 0) {
+				returnValid.push_back("Passed Odd");
+				passed[2] = true;
 			}
+			else {
+				returnValid.push_back("Failed Odd");
+			}
+		}
+		else if (direction == "ANY") {
+			returnValid.push_back("No Direction restriction");
+			passed[2] = true;
+		}
+		else {
+			string errorText{ "Config Error for Even/Odd on SID: " };
+			errorText += first_wp;
+			sendMessage("Error", errorText);
+			returnValid.push_back("Config Error for Even/Odd on this SID!");
+		}
+		
+		// Flight level
+		int min_fl, max_fl;
+		if (conditions[i].HasMember("min_fl") && (min_fl = conditions[i]["min_fl"].GetInt()) > 0) {
+			if ((RFL / 100) >= min_fl) {
+				returnValid.push_back("Passed Minimum Flight Level");
+				passed[3] = true;
+			}
+			else {
+				returnValid.push_back("Failed Minimum Flight Level");
+			}
+		}
+		else {
+			returnValid.push_back("No Minimum Flight Level");
+			passed[3] = true;
+		}
+
+		if (conditions[i].HasMember("max_fl") && (max_fl = conditions[i]["max_fl"].GetInt()) > 0) {
+			if ((RFL / 100) <= max_fl) {
+				returnValid.push_back("Passed Maximum Flight Level");
+				passed[4] = true;
+			}
+			else {
+				returnValid.push_back("Failed Maximum Flight Level");
+			}
+		}
+		else {
+			returnValid.push_back("No Maximum Flight Level");
+			passed[4] = true;
+		}
+
+
+
+		bool passedVeri{ false };
+		for (int i = 0; i < 5; i++) {
+			if (passed[i])
+			{
+				passedVeri = true;
+			}
+			else {
+				passedVeri = false;
+				break;
+			}
+		}
+		if (passedVeri) {
+			returnValid.push_back("Passed");
 			break;
 		}
+		else {
+			returnValid.push_back("Failed");
+			if (!passed[0] || !passed[1])
+				continue;
+			else
+				break;
+		}
 	}
+	
 	if (!valid) {
 		returnValid.push_back("Invalid");
 		returnValid.push_back("No valid SID found!");
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 5; i++) {
 			returnValid.push_back("-");
 		}
+		returnValid.push_back("Failed");
 	}
 	return returnValid;
 }
 
+//
 void CVFPCPlugin::OnFunctionCall(int FunctionId, const char * ItemString, POINT Pt, RECT Area) {
 	if (FunctionId == TAG_FUNC_CHECKFP_MENU) {
 		OpenPopupList(Area, "Check FP", 1);
@@ -277,19 +321,21 @@ void CVFPCPlugin::OnGetTagItem(CFlightPlan FlightPlan, CRadarTarget RadarTarget,
 		if (fpType == "V") {
 			*pRGB = TAG_GREEN;
 			strcpy_s(sItemString, 16, "VFR");
-		} else {
-			for (int i = 0; i < sidName.size(); i++) {
-				vector<string> messageBuffer{ validizeSid(FlightPlan) };		// 0 = Callsign, 1 = valid/invalid SID, 2 = SID Name, 3 = Even/Odd, 4 = Minimum Flight Level, 5 = Maximum Flight Level, 6 = Passed
-				if (messageBuffer.at(6) == "Passed") {
-					*pRGB = TAG_GREEN;
-					strcpy_s(sItemString, 16, "OK!");
-					break;
-				}
-				else {
-					*pRGB = TAG_RED;
-					string code = getFails(validizeSid(FlightPlan));
-					strcpy_s(sItemString, 16, code.c_str());
-				}
+		}
+		else {
+			vector<string> messageBuffer{ validizeSid(FlightPlan) };	// 0 = Callsign, 1 = valid/invalid SID, 2 = SID Name, 3 = Even/Odd, 4 = Minimum Flight Level, 5 = Maximum Flight Level, 6 = Passed
+
+			if (messageBuffer.size() < 9)
+				int a = 1; // DEBUG BREAK
+
+			if (messageBuffer.at(8) == "Passed") {
+				*pRGB = TAG_GREEN;
+				strcpy_s(sItemString, 16, "OK!");
+			}
+			else {
+				*pRGB = TAG_RED;
+				string code = getFails(validizeSid(FlightPlan));
+				strcpy_s(sItemString, 16, code.c_str());
 			}
 		}
 
@@ -318,9 +364,10 @@ bool CVFPCPlugin::OnCompileCommand(const char * sCommandLine) {
 		return true;
 	}
 	if (startsWith(".vfpc load", sCommandLine)) {
+		locale loc;
 		string buffer{ sCommandLine };
 		buffer.erase(0, 11);
-		getSids(buffer);
+		getSids();
 		return true;
 	}
 	if (startsWith(".vfpc check", sCommandLine))
@@ -333,16 +380,16 @@ bool CVFPCPlugin::OnCompileCommand(const char * sCommandLine) {
 
 // Sends to you, which checks were failed and which were passed on the selected aircraft
 void CVFPCPlugin::checkFPDetail() {	
-	vector<string> messageBuffer{ validizeSid(FlightPlanSelectASEL()) };		// 0 = Callsign, 1 = valid/invalid SID, 2 = SID Name, 3 = Even/Odd, 4 = Minimum Flight Level, 5 = Maximum Flight Level, 6 = Passed
+	vector<string> messageBuffer{ validizeSid(FlightPlanSelectASEL()) };	// 0 = Callsign, 1 = valid/invalid SID, 2 = SID Name, 3 = Even/Odd, 4 = Minimum Flight Level, 5 = Maximum Flight Level, 6 = Passed
 	sendMessage(messageBuffer.at(0), "Checking...");
 	string buffer{ messageBuffer.at(1) };
 	if (messageBuffer.at(1) == "Valid") {
 		buffer += ", found SID: ";
-		for (int i = 2; i < 6; i++) {
+		for (int i = 2; i < 8; i++) {
 			buffer += messageBuffer.at(i);
 			buffer += ", ";
 		}
-		buffer += messageBuffer.at(6);
+		buffer += messageBuffer.at(8);
 		buffer += " FlightPlan Check. Check complete.";
 	} else {
 		buffer += " ";
@@ -353,48 +400,52 @@ void CVFPCPlugin::checkFPDetail() {
 }
 
 string CVFPCPlugin::getFails(vector<string> messageBuffer) {
-	string fail[4];
+	vector<string> fail;
+	fail.push_back("FPL");
+
 	if (messageBuffer.at(1) == "Invalid") {
-		fail[0] = "SID";
-	} else {
-		fail[0] = "FPL";
+		fail.push_back("SID");
 	}
 	if (messageBuffer.at(3).find_first_of("Failed") == 0) {
-		fail[1] = "E/O";
-	} else {
-		fail[1] = "FPL";
+		fail.push_back("DST");
 	}
 	if (messageBuffer.at(4).find_first_of("Failed") == 0) {
-		fail[2] = "MIN";
-	} else {
-		fail[2] = "FPL";
+		fail.push_back("AWY");
 	}
+
 	if (messageBuffer.at(5).find_first_of("Failed") == 0) {
-		fail[3] = "MAX";
-	} else {
-		fail[3] = "FPL";
+		fail.push_back("E/O");
 	}
-	return fail[disCount];
+	if (messageBuffer.at(6).find_first_of("Failed") == 0) {
+		fail.push_back("MIN");
+	}
+	if (messageBuffer.at(7).find_first_of("Failed") == 0) {
+		fail.push_back("MAX");
+	}
+
+	int couldnt = disCount;
+	while (couldnt >= fail.size())
+		couldnt -= fail.size();
+	return fail[couldnt];
 }
 
 void CVFPCPlugin::OnTimer(int Counter) {
 
 	blink = !blink;
 
-	if (disCount < 3) {
-		disCount++;
-	} else {
-		disCount = 0;
+	if (blink) {
+		if (disCount < 3) {
+			disCount++;
+		}
+		else {
+			disCount = 0;
+		}
 	}
 
 	// Loading proper Sids, when logged in
 	if (GetConnectionType() != CONNECTION_TYPE_NO && !initialSidLoad) {
 		string callsign{ ControllerMyself().GetCallsign() };
-		if (callsign.find_first_of('_O') == 3) {
-			sendMessage("Observer Mode, no SIDs loaded");
-		} else {	
-			getSids("");
-		}
+		getSids();
 		initialSidLoad = true;
 	} else if (GetConnectionType() == CONNECTION_TYPE_NO && initialSidLoad) {
 		sidName.clear();
--- analyzeFP.hpp
+++ analyzeFP.hpp
@@ -1,20 +1,27 @@
 #pragma once
 #include "EuroScopePlugIn.h"
+#include <sstream>
 #include <iostream>
 #include <string>
 #include "Constant.hpp"
 #include <fstream>
 #include <vector>
+#include <map>
+#include <boost/algorithm/string.hpp>
+#include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
 
 #define MY_PLUGIN_NAME      "VFPC"
-#define MY_PLUGIN_VERSION   "1.4"
-#define MY_PLUGIN_DEVELOPER "Jan Fries"
+#define MY_PLUGIN_VERSION   "2.0"
+#define MY_PLUGIN_DEVELOPER "Jan Fries, Hendrik Peter"
 #define MY_PLUGIN_COPYRIGHT "GPL v3"
 #define MY_PLUGIN_VIEW_AVISO  "Vatsim FlightPlan Checker"
 
 #define PLUGIN_WELCOME_MESSAGE	"Willkommen beim Vatsim Flugplan-RFL checker"
 
 using namespace std;
+using namespace boost;
+using namespace rapidjson;
 using namespace EuroScopePlugIn;
 
 class CVFPCPlugin :
@@ -24,7 +31,7 @@ public:
 	CVFPCPlugin();
 	virtual ~CVFPCPlugin();
 
-	virtual void getSids(string airport);
+	virtual void getSids();
 
 	virtual vector<string> validizeSid(CFlightPlan flightPlan);
 
@@ -40,6 +47,40 @@ public:
 		COLORREF* pRGB,
 		double* pFontSize);
 
+	template <typename Out>
+	void split(const string& s, char delim, Out result) {
+		istringstream iss(s);
+		string item;
+		while (getline(iss, item, delim)) {
+			*result++ = item;
+		}
+	}
+
+	vector<string> split(const string& s, char delim) {
+		vector<string> elems;
+		split(s, delim, back_inserter(elems));
+		return elems;
+	}
+
+	bool arrayContains(const Value& a, string s) {
+		for (SizeType i = 0; i < a.Size(); i++) {
+			if (a[i].GetString() == s)
+				return true;
+		}
+		return false;
+	}
+
+	bool routeContains(string s, const Value& a) {
+		for (SizeType i = 0; i < a.Size(); i++) {
+			bool dd = contains(s, a[i].GetString());
+			if (contains(s, "BRANE"))
+				int a = 1;
+			if (contains(s, a[i].GetString()))
+				return true;
+		}
+		return false;
+	}
+
 	virtual bool OnCompileCommand(const char * sCommandLine);
 
 	virtual void debugMessage(string type, string message);
@@ -53,5 +94,9 @@ public:
 	virtual string getFails(vector<string> messageBuffer);
 
 	virtual void OnTimer(int Count);
+
+protected:
+	Document config;
+	map<string, rapidjson::SizeType> airports;
 };
 
